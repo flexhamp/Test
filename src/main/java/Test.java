8b3143f5084374d93363c5310281a7abc9014162
Kuzmenko Nikita <flexhamp@gmail.com>
2018-03-12 15:50:13 +0700

    Commit - 1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,6 +1,8 @@
 public class Test {
-    public Test(){
+    public Test() {
         System.out.println("Hellow! I'm Test.class");
         System.out.println("new branch");
+
+        String s = new String("new String");
     }
 }
