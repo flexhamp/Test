7a00fa5e0184da3deaf14bc785eb3a392cdf3dfe
Kuzmenko Nikita <flexhamp@gmail.com>
2018-03-20 16:58:18 +0700

    Начал писать компаратор для сравнения Individual

--- src/main/java/Lambda.java
+++ src/main/java/Lambda.java
@@ -1,77 +1,93 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Lambda {
+    private static SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
 
-    public static void main(String[] args) {
-        BirthDate birthDate = new BirthDate();
-        birthDate.setValue(new Date());
-        IndividualName individualName = new IndividualName();
-        individualName.setFirstName("Test1");
-        individualName.setLastName("Testov1");
-        individualName.setSecondName("Testovich1");
-
-        List<IndividualName> names = new ArrayList<>();
-        names.add(individualName);
-
-        Individual individual = new Individual();
-        individual.setNames(names);
-        individual.setBirthDate(birthDate);
-
+    public static void main(String[] args) throws ParseException {
+        //BirthDate
         BirthDate birthDate1 = new BirthDate();
-        birthDate.setValue(new Date());
+        birthDate1.setValue(df.parse("22-03-18"));
+
+        BirthDate birthDate2 = new BirthDate();
+        birthDate2.setValue(new Date());
+
+        BirthDate birthDate3 = new BirthDate();
+        birthDate3.setValue(new Date());
+
+        //IndividualName
         IndividualName individualName1 = new IndividualName();
-        individualName1.setFirstName("Test1");
-        individualName1.setLastName("Testov1");
-        individualName1.setSecondName("Testovich1");
+        individualName1.setFirstName("Test2");
+        individualName1.setLastName("Testov2");
+        individualName1.setSecondName("Test1");
+
+        IndividualName individualName2 = new IndividualName();
+        individualName2.setFirstName("Test2");
+        individualName2.setLastName("Testov2");
+        individualName2.setSecondName("Test2");
+
+        IndividualName individualName3 = new IndividualName();
+        individualName3.setFirstName("Test3");
+        individualName3.setLastName("Testov3");
+        individualName3.setSecondName("Test3");
 
         List<IndividualName> names1 = new ArrayList<>();
-        names.add(individualName);
+        names1.add(individualName1);
 
+        List<IndividualName> names2 = new ArrayList<>();
+        names2.add(individualName2);
+
+        List<IndividualName> names3 = new ArrayList<>();
+        names3.add(individualName2);
+        names3.add(individualName3);
+
+        //Individual 1
         Individual individual1 = new Individual();
         individual1.setNames(names1);
         individual1.setBirthDate(birthDate1);
 
-//        System.out.println(individual);
-//        System.out.println(individual1);
+        //Individual 2
+        Individual individual2 = new Individual();
+        individual2.setNames(names2);
+        individual2.setBirthDate(birthDate2);
 
-        System.out.println(individualComparator("").compare(individual, individual1));
-        collectionsComparator(IndividualName)
+        //Individual 3
+        Individual individual3 = new Individual();
+        individual3.setNames(names3);
+        individual3.setBirthDate(birthDate3);
 
+        System.out.println(individualComparator("").compare(individual3, individual1));
+
+        System.out.println(dateComparator().compare(individual3, individual2));
     }
-
-    public static double calculate(double a, double b, Action action){
-        return action.operation(a, b);
-    }
-
-    @FunctionalInterface
-    interface Action{
-        double operation(double a, double b);
-    }
-
-
-    public static Comparator<Individual> individualComparator(String searchStrategy) {
-        Comparator<Individual> comp = Comparator.comparing(Individual::getNames, collectionsComparator(namesComparator()));
-        if (searchStrategy == "sd") {
-            comp = comp.thenComparing(Individual::getBirthDate, Comparator.comparing(BirthDate::getValue));
-        }
-        return comp;
-    }
+    
 
     private static <T> Comparator<Collection<T>> collectionsComparator(Comparator<T> elemComparator) {
         return (col1, col2) -> {
-            System.out.println(col1);
-            System.out.println(col2);
             TreeSet<T> treeSet = new TreeSet<>(elemComparator);
-            treeSet.forEach(System.out::println);
             treeSet.addAll(col1);
             return treeSet.containsAll(col2) ? 0 : 1;
         };
     }
 
+    public static Comparator<Individual> individualComparator(String searchStrategy) {
+        Comparator<Individual> comp = Comparator.comparing(Individual::getNames, collectionsComparator(namesComparator()))
+                .thenComparing(dateComparator());
+
+        return comp;
+    }
+
+
     public static Comparator<IndividualName> namesComparator() {
-        return Comparator.comparing(IndividualName::getFirstName).thenComparing(IndividualName::getLastName);
+        return Comparator.comparing(IndividualName::getFirstName)
+                .thenComparing(IndividualName::getLastName);
+    }
+
+    public static Comparator<Individual> dateComparator() {
+        return Comparator.comparing(Individual::getBirthDate, Comparator.comparing(BirthDate::getValue));
     }
 
 
