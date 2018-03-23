1fee1a4eb1097a037b02682f7f565fec1851361a
Kuzmenko Nikita <flexhamp@gmail.com>
2018-03-23 10:58:41 +0700

    Перегрузил метод collectionsComparator

--- src/main/java/Lambda.java
+++ src/main/java/Lambda.java
@@ -1,6 +1,7 @@
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 
 public class Lambda {
@@ -47,22 +48,22 @@ public class Lambda {
         individualName4.setSecondName("Test4");
 
         IndividualName individualName5 = new IndividualName();
-        individualName5.setFirstName("Test5");
-        individualName5.setLastName("Testov5");
-        individualName5.setSecondName("Test5");
+        individualName5.setFirstName("Test1");
+        individualName5.setLastName("Testov1");
+        individualName5.setSecondName("Test1");
 
         List<IndividualName> names1 = new ArrayList<>();
         names1.add(individualName1);
 
         List<IndividualName> names2 = new ArrayList<>();
         names2.add(individualName2);
-        names2.add(individualName1);
+        names2.add(individualName5);
 
         List<IndividualName> names3 = new ArrayList<>();
-        names3.add(individualName2);
-        names3.add(individualName4);
-        names3.add(individualName5);
+        names3.add(individualName3);
         names3.add(individualName1);
+        names3.add(individualName5);
+
 
         List<IndividualName> names4 = new ArrayList<>();
         names4.add(individualName5);
@@ -102,10 +103,18 @@ public class Lambda {
 
         List<Individual> list = Arrays.asList(individual2, individual3, individual4, individual5);
 
-        System.out.println(collectionsComparator(individualComparator()).compare(Arrays.asList(individual1), list));
+        System.out.println(collectionsComparator("").compare(Arrays.asList(individual1), list));
 //        System.out.println(dateComparator().compare(individual3, individual2));
     }
 
+
+    public static <T> Comparator<Collection<Individual>> collectionsComparator(String elemComparator) {
+        return (col1, col2) -> {
+            TreeSet<Individual> treeSet = new TreeSet<>(individualComparator());
+            treeSet.addAll(col1);
+            return treeSet.containsAll(col2) ? 0 : 1;
+        };
+    }
 
     public static <T> Comparator<Collection<T>> collectionsComparator(Comparator<T> elemComparator) {
         return (col1, col2) -> {
@@ -115,7 +124,7 @@ public class Lambda {
         };
     }
 
-    public static Comparator<Individual> individualComparator() {
+    private static Comparator<Individual> individualComparator() {
         return Comparator.comparing(Individual::getNames, collectionsComparator(uniComparator(
                 IndividualName::getFirstName,
                 individualName1 -> normalize(individualName1.getLastName()),
@@ -128,7 +137,7 @@ public class Lambda {
     }
 
 
-    public static <T, R extends Comparable> Comparator<T> uniComparator(Function<T, R>... thenComparing) {
+    private static <T, R extends Comparable> Comparator<T> uniComparator(Function<T, R>... thenComparing) {
         Comparator<T> comparator = Comparator.comparing(thenComparing[0]);
         for (int i = 1; i < thenComparing.length; i++) {
             comparator = comparator.thenComparing(thenComparing[i]);
